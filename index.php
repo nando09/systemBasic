7f579c0d9b57366dd9cef66b30cfb3fb14e86a41
nando09 <nando.online.fb@gmail.com>
2019-01-22 17:16:11 -0200

    Insere os concorrentes no index

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!-- ************************************ SITES QUE SÃO REFERENCIAS DE SISTEMA DE ESTOQUE DOS CONCORRENTES:  ************************************ -->
+<!-- https://hiper.com.br/ -->
+<!-- https://www.youtube.com/watch?v=0Txv593x1Bs -->
+<!-- https://vhsys.com.br/ -->
+<!-- https://contaazul.com -->
+<!-- https://www.programanex.com.br/recursos/controle-de-estoque -->
+<!-- https://www.egestor.com.b -->
+<!-- https://gestaoclick.com.br -->
+<!-- https://www.westoque.com.br/ -->
+<!-- https://app.westoque.com.br/olateste/index user: olateste; senha: teste123321 -->
+<!-- https://www.sagestart.com.br/controle-de-estoque/ -->
+<!-- https://marketup.com/ -->
+<!-- https://www.tagplus.com.br/ -->
+
+<!-- ************************************ PESQUISAR SOBRE  ************************************ -->
+<!-- NFC-e, NF-e, NFS-e, CT-e -->
+
+<!-- ************************************ POSTGRE COM ARRAY:  ************************************ -->
+<!-- https://www.youtube.com/watch?v=c8dXZuGa5wI -->
+
+<!-- ************************************ SCRIPTS DE SISTEMA JÁ FEITOS:  ************************************ -->
+<!-- https://www.scriptbrasil.com.br/codigos/php/ -->
 <?php
 
 	include 'helper/autoLoad.php';
