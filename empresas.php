c800036c409085c44935fb21174ef9f3c623e5ad
nando09 <nando.online.fb@gmail.com>
2019-01-29 17:34:45 -0200

    Liks para os videos

--- empresas.php
+++ empresas.php
@@ -16,7 +16,60 @@
 	https://www.westpharma.com/
 
 
+	
+	***************** CLUOD AWS *****************
+	https://www.youtube.com/watch?v=ZkSgKN53NaI
+
 	***************** NOMES *****************
 	Qualyplas Indústria Comércio Artefatos Plásticos
 	Proinject
 	Quimar Indústrias Químicas
+
+
+	***************** IDEIAS *****************
+	RELATORIO DO CLIENTE
+			->	Desde de quando cliente não compra mais!
+			->	Mandar um email para ele!
+			->	Quais os produtos ele já comprou.
+			*	A ideia é a seguinte: O usuario pode ver quais são os clientes que não estão comprando mais, e o bom sera que ele irar ver qual os produtos que esse cliente comprou para oferecer da proxima vez com descontos ou algo mais.
+
+	RELATORIO DO FORNECEDOR
+			->	Os produtos oferecidos e o valor que ira comprar
+			->	O mesmo produto com outros fornecedores e valores no detalhe
+			*	Podemos entrar em contatos com os fornecedores informando os valores que o concorrentes deles estão com um valor abaixo.
+
+	RELATORIO DO PRODUTO
+			->	Peça parada no estoque
+			->	Peça com mais venda
+			->	Lucro da data informada
+			->	Fornecedores que vedem o produto
+			->	Clientes que compraram
+			*	Mostrando os produtos que não estão saindo o usuario poderar dar uma oferta ao seus clientes que já compraram e informar que o produto esta com desconto ou algo que preferir 
+
+	NOTICIA
+			->	Mostra todos os planos que estão chegando e aprimorando
+			->	Fala quem esta entrando ou alterando o plano e o que faz essa empresa
+			*	Que os usuarios entre em acordo para saber sobre o plano
+
+	ENTRADA
+			->	Aqui sera a conversa com o suporte
+			->	Entrada de suporte sera ligada um protocolo
+			->	Para entrar em contato com o suporte tem que clicar em suporte
+			*	Que o atendimento seja controlado pela parte de entrada caso seja um acompanhamento mais flexivel
+
+	PROJETO
+			->	Ira colocar os projetos que querem que seja atualizado na empresa
+			*	As vezes esquecemos de ideias boas e quando vamos lembra já poderiamos esta com os carros muito a frente.
+
+
+
+	***** NOME *****
+	Diana Arnos => Programadora PHP
+	Cloud Treinamentos => YOUTUBE
+
+	**** FATEC ****
+	https://www.vestibularfatec.com.br/unidades-cursos/
+	http://www.sp.senac.br/graduacao/curso/tecnologia/sistemas-para-internet
+	https://www.youtube.com/watch?v=fEfdjg5vW3I
+	https://www.uninove.br/video-do-curso-tecnologia-em-sistemas-para-internet/
+
