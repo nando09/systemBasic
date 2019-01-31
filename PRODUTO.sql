bb59b0b2464e6e6171e96acae2127256b0a61890
nando09 <nando.online.fb@gmail.com>
2019-01-31 17:30:57 -0200

    inclundo comandos no SQL

--- PRODUTO.sql
+++ PRODUTO.sql
@@ -100,6 +100,8 @@ DELETE FROM USUARIO WHERE id = 13;
 UPDATE usuario SET nome = 'João',usuario = 'Joção', senha = MD5('joão') WHERE id = 12;
 UPDATE usuario SET nome = 'Filipinas',usuario = 'Doidão', senha = MD5('açucar') WHERE id = 10;
 
+SELECT NOME,USUARIO,SENHA FROM USUARIO WHERE USUARIO = 'nando' AND SENHA = MD5('123321');
+
 INSERT INTO USUARIO (NOME, USUARIO, SENHA) VALUES ('Fernando', 'nando', MD5('123321'));
 INSERT INTO USUARIO (NOME, USUARIO, SENHA) VALUES ('Jaqueline', 'jaque', MD5('jaque'));
 INSERT INTO USUARIO (NOME, USUARIO, SENHA) VALUES ('Mel', 'mel', MD5('mel'));
