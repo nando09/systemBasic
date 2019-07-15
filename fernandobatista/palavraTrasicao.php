10fe8c5f561e046c5e0d891fd058dc5af9fd3396
nando09 <nando.online.fb@gmail.com>
2019-07-15 16:52:29 -0300

    Curriculo

---
+++ fernandobatista/palavraTrasicao.php
@@ -0,0 +1,6 @@
+Enumeração				=>		primeiro de tudo, também, outro, além disso, finalmente
+Causa					=>		porque, assim, devido a, pela razão que
+Comparação / contraste	=>		da mesma forma, do mesmo modo, enquanto, em contraste
+Conclusão				=>		como resultado, conseqüentemente, conseqüentemente,
+Sinais					=>		difusos	parece que, talvez, provavelmente, quase
+Ênfase					=>		acima de tudo, mais digno de nota, certamente, ainda mais
