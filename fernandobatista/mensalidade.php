3a416f59b0a4c792b12d0118564e9a0a3a891809
nando09 <nando.online.fb@gmail.com>
2019-07-24 17:34:17 -0300

    Estudar

---
+++ fernandobatista/mensalidade.php
@@ -0,0 +1,17 @@
+<?php
+	$internet				=		50 	por mes;
+	$desodorante			=		30 	por mes;
+	$prestobarba			=		60 	dura uns 3 meses : 20 por mes;
+	$sabonete				=		20 	por mes;
+	$pastaDente				=		20 	por mes;
+	$shamCondci				=		40 	por mes;
+	$gas					=		90 	dura uns 3 meses : 30 por mes;
+	$agua					=		60 	por mes;
+	$luz					=		120 por mes;
+	$carne					=		200 por mes;
+	$arrozFeijao			=		200 por mes;
+
+	$ração					=		100 dura uns 2 meses : 50 por mes;
+	$banho					=		70 por mes;
+	$remedio				=		100 dura uns 3 meses ;
+?>
